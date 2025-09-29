0898ebbe283461c8ad245693ea5472175117fd87
公彦赤屋先 <kimihikoakaya@gmail.com>
2025-09-30 01:44:58 +0900

    Modernisation, linux support

---
+++ Samples/NiTE.ConsoleTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+using NiTEWrapper;
+using OpenNIWrapper;
+
+Console.WriteLine("Initializing NiTE library");
+
+if (NiTE.Initialize() is not NiTE.Status.Ok)
+{
+    Console.WriteLine("Error initializing NiTE");
+    return;
+}
+
+Console.WriteLine($"Running NiTE v{NiTE.Version}");
+
+var tracker = UserTracker.Create();
+tracker.OnNewData += userTracker =>
+{
+    if (!userTracker.IsValid) return;
+    using var frame = userTracker.ReadFrame();
+    foreach (var user in frame.Users)
+        if (user.IsNew)
+        {
+            Console.WriteLine($"New user {user.UserId}");
+            userTracker.StartSkeletonTracking(user.UserId);
+        }
+        else if (user.IsLost)
+        {
+            Console.WriteLine($"Lost user {user.UserId}");
+        }
+        else if (user.Skeleton.State == Skeleton.SkeletonState.Tracked)
+        {
+            var head = user.Skeleton.GetJoint(SkeletonJoint.JointType.Head);
+            Console.WriteLine($"{user.UserId}: {head.Position.ToVector()}");
+        }
+};
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+Console.WriteLine("Shutting down...");
+NiTE.Shutdown();
+OpenNI.Shutdown();
+
+public static class Extensions
+{
+    public static Vector3 ToVector(this Point3D point)
+    {
+        return new Vector3(point.X, point.Y, point.Z) * 0.001f;
+    }
+}
--- Samples/NiTE.SkeletonTracker/Program.cs
+++ Samples/NiTE.SkeletonTracker/Program.cs
@@ -1,27 +1,11 @@
-﻿using System;
-using System.Windows.Forms;
+namespace NiTE.SkeletonTracker;
 
-namespace NiTESkeletonTracker
+internal static class Program
 {
-    #region
-
-    #endregion
-
-    internal static class Program
+    [STAThread]
+    private static void Main()
     {
-        #region Methods
-
-        /// <summary>
-        ///     The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        private static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Main());
-        }
-
-        #endregion
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Main());
     }
 }
